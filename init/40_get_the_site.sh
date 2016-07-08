76bcf571120b7d57bfe9b3ab65f652d016bd591c
Rajitha Perera <rajiteh@users.noreply.github.com>
2016-07-07 22:34:42 -0400

    Update 40_get_the_site.sh

--- init/40_get_the_site.sh
+++ init/40_get_the_site.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-[[ ! -d /config/hydra/.git ]] && (git clone https://github.com/theotherp/nzbhydra /config/hydra && chown -R abc:abc /config)
+HYDRA_RUN_PATH=/hydra
+mkdir -p $HYDRA_RUN_PATH
+[[ ! -d $HYDRA_RUN_PATH/.git ]] && (git clone https://github.com/theotherp/nzbhydra $HYDRA_RUN_PATH && chown -R abc:abc $HYDRA_RUN_PATH)
 
 # test if updates have been disabled
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
-cd /config/hydra
+cd $HYDRA_RUN_PATH
 git pull
-chown -R abc:abc /config
+chown -R abc:abc $HYDRA_RUN_PATH
